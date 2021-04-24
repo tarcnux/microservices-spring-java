29f6cf620ac39bf274c4dae1ccf7d17acadc30b4
Tarcisio Nunes <github@tarcnux.com.br>
2021-04-24 00:01:10 -0300

    RestTemplate

--- hr-payroll/src/main/java/br/com/tarcnux/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/tarcnux/hrpayroll/services/PaymentService.java
@@ -1,12 +1,31 @@
 package br.com.tarcnux.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import br.com.tarcnux.hrpayroll.entities.Payment;
+import br.com.tarcnux.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
+	
+	@Value("${hr-worker.host}")	
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
+	
 	public Payment getPayment(long workerID, int days) {
-		return new Payment("Bob", 200.0, days);
+		
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", ""+workerID);//conversão para string
+		
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }

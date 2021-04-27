12791db33be057b1375e46077816440cc03a9a67
Tarcisio Nunes <github@tarcnux.com.br>
2021-04-27 20:40:40 -0300

    Project hr-api-gateway-zuul created

---
+++ hr-api-gateway-zuul/src/test/java/br/com/tarcnux/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tarcnux.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
